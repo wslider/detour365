0229f2e86d80a5397ac448a1db06eb081bda07d9
wslider <wslider2000@gmail.com>
2025-07-17 14:49:49 -0400

    updated updateMainImage function to change alt of image to match updated image

--- script.js
+++ script.js
@@ -19,11 +19,16 @@ function updateMainImage (){
     const mainImage = document.getElementById('mainImage'); 
      if (hour >= 6 && hour <= 15) { 
         mainImage.src='https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/paths-to-the-top-william-slider.jpg';
+        mainImage.alt='mountain with path starting at the base forking into two hiking paths to the top';
     }
     else if (hour <19) {
         mainImage.src='https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/smoky-mountain-sunset-from-clingmans-dome-william-slider.jpg';
+        mainImage.alt='sunset in the smoky mountains viewed from Clingmans Dome';
     }
-    else {mainImage.src='https://images.fineartamerica.com/images/artworkimages/mediumlarge/3/night-forest-silhouettes-william-slider.jpg';}
+    else {
+        mainImage.src='https://images.fineartamerica.com/images/artworkimages/mediumlarge/3/night-forest-silhouettes-william-slider.jpg';
+        mainImage.alt-'night sky over silhouettes of evergreen trees';
+    }
 }
 
 updateMainImage(); 
