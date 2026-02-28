c11b7728e3c191bbb7ac7f9036760534b55d514f
wslider <wslider2000@gmail.com>
2026-02-27 20:35:22 -0500

    created daily.json and daily.js for verse of the day feature

---
+++ js/daily.js
@@ -0,0 +1,4 @@
+//display verse of the day using data from daily.json - match to the current month and day, ignoring the year
+//display verse in a blockquote with the verse reference below it
+//display over image of the day with a dark overlay for readability
+//use the verse of the day as the alt text for the image
