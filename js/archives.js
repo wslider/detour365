7c2fc4db27daae81e2a123891ec253e6f0828b3b
wslider <wslider2000@gmail.com>
2026-03-30 16:21:55 -0400

    initial MVP version of timeline on about.html

---
+++ js/archives.js
@@ -0,0 +1,21 @@
+
+// search with submit event listener 
+
+// take input from search > clean input 
+
+// fetch using clean input as search term 
+
+// async fetch all matches from youtube rss
+
+    // display embedded - for each: create a div element with iframe 
+
+// async fetch  all matches from spotifty rss
+
+    // display embedded - for each: create a div element
+
+// async fetch from X (future)
+
+    // display - for each create a div element
+
+// call 
+
